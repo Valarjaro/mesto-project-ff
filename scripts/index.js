3e201c14a7edae09bbe98d2942e58c585c315e92
Valarjaro <Valarjar05@yandex.ru>
2025-02-24 17:47:33 +0300

    Вывел одну карточку

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector('.places__list');
+
+const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+cardElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+cardElement.querySelector('.card__title').textContent = 'Архыз';
+
+placesList.append(cardElement);
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
