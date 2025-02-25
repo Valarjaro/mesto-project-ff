d98c2c66a5c2cf42a64de7307e94100387ca18c0
Valarjaro <Valarjar05@yandex.ru>
2025-02-25 11:07:21 +0300

    @todo: Функция удаления карточки + Скорректировал функцию создания карточки, вывод карточек на страницу. Readme +

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Valarjaro/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -8,24 +8,37 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function cardAdd (cardTitleSource, cardImageSource) {
+// принимает в аргументах данные одной карточки и функцию-колбэк для удаления
+function cardAdd (cardDataSource, cardRemoveCallback) {
+    // клонировать шаблон,
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
 
-    cardImage.setAttribute('src', cardImageSource);  
-    cardImage.setAttribute('alt', cardTitleSource);
+    // установить значения вложенных элементов,
+    cardImage.src = cardDataSource.link;  
+    cardImage.alt = cardDataSource.name;
 
-    cardTitle.textContent = cardTitleSource;
+    cardTitle.textContent = cardDataSource.name;
 
+    // добавить к иконке удаления обработчик клика, по которому будет вызван переданный в аргументах колбэк.
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+        cardRemoveCallback(cardElement);
+    });
+
+    // возвращает подготовленный к выводу элемент карточки
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
 
+function сardRemove(card) {
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(function(data) {
-    placesList.append(cardAdd(data.name, data.link));
+initialCards.forEach((cardDataSource) => {
+  placesList.append(cardAdd (cardDataSource, сardRemove));
 });
