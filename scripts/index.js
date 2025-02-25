769fe24cf61d5a9dac7b161ff5bb24c519f94450
Valarjaro <Valarjar05@yandex.ru>
2025-02-25 09:09:58 +0300

    @todo: Функция создания карточки + @todo: Вывести карточки на страницу +
    Все карточки выведены

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,31 @@
 // @todo: Темплейт карточки
+
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 const placesList = document.querySelector('.places__list');
 
-const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-
-cardElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-cardElement.querySelector('.card__title').textContent = 'Архыз';
-
-placesList.append(cardElement);
 // @todo: Функция создания карточки
+
+function cardAdd (cardTitleSource, cardImageSource) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+
+    cardImage.setAttribute('src', cardImageSource);  
+    cardImage.setAttribute('alt', cardTitleSource);
+
+    cardTitle.textContent = cardTitleSource;
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(data) {
+    placesList.append(cardAdd(data.name, data.link));
+});
