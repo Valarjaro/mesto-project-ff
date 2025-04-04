9421c699cb46371d28525cb42842d2159bb6f1e0
Valarjaro <Valarjar05@yandex.ru>
2025-04-04 17:09:10 +0300

    Валидация формы «Редактировать профиль» done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Проектная работа Mesto
 
 https://github.com/Valarjaro/mesto-project-ff.git
+
+https://valarjaro.github.io/mesto-project-ff
--- src/index.js
+++ src/index.js
@@ -128,3 +128,82 @@ formNewPlace.addEventListener("submit", function(evt){
     //и очищалась форма.
     formNewPlace.reset();
 });
+
+// 7 спринт
+
+// 1. Валидация формы «Редактировать профиль»
+
+// enableValidation({
+//     formSelector: '.popup__form',
+//     inputSelector: '.popup__input',
+//     submitButtonSelector: '.popup__button',
+//     inactiveButtonClass: 'popup__button_disabled',
+//     inputErrorClass: 'popup__input_type_error',
+//     errorClass: 'popup__error_visible'
+//   });
+
+
+
+  function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+    inputElement.classList.add('popup__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+  };
+
+  function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+
+    inputElement.classList.remove('popup__input_type_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+  };
+
+  function isValid(formElement, inputElement) {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    }
+    else {
+        hideInputError(formElement, inputElement);
+    }
+  };
+
+  function setEventListener(formElement) {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+  };
+
+  function enableValidation() {
+    const formList = Array.from(document.querySelectorAll('.popup__form'))
+    formList.forEach((formElement) => {
+        setEventListener(formElement);
+  });
+};
+  enableValidation();
+ 
+
+  function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {return !inputElement.validity.valid;})
+  };
+
+  function toggleButtonState(inputList, buttonElement) {
+    if (hasInvalidInput(inputList)) {
+        // сделай кнопку неактивной
+        buttonElement.disabled = true;
+        buttonElement.classList.add('popup__button_disabled');
+      } else {
+        // иначе сделай кнопку активной
+        buttonElement.disabled = false;
+        buttonElement.classList.remove('popup__button_disabled');
+      }
+  };
+
+  // 1. Валидация формы «Редактировать профиль»
